5046f033eb25265b92600520833b9bb942bfbb70
Fortunatus A. Wulnye <56046745+Fortune-Labs@users.noreply.github.com>
2020-08-21 15:51:33 +0000

    Update commentDetail.js

--- reactjsx/src/commentDetail.js
+++ reactjsx/src/commentDetail.js
@@ -2,59 +2,28 @@ import React from "react";
 
 // converted to class-based component
 
-const CommentDetail = (props) => {
-  return _react.default.createElement(
-    "div",
-    {
-      className: "comment",
-    },
-    _react.default.createElement(
-      "a",
-      {
-        href: "/",
-        className: "avatar",
-      },
-      _react.default.createElement("img", {
-        alt: "avatar",
-        src: props.img,
-      })
-    ),
-    _react.default.createElement(
-      "div",
-      {
-        className: "content",
-      },
-      _react.default.createElement(
-        "a",
-        {
-          href: "/",
-          className: "author",
-        },
-        props.author
-      ),
-      _react.default.createElement(
-        "div",
-        {
-          className: "metadata",
-        },
-        _react.default.createElement(
-          "span",
-          {
-            className: "date",
-          },
-          props.timeAgo
-        )
-      ),
-      _react.default.createElement(
-        "div",
-        {
-          className: "text",
-        },
-        props.commentText
-      )
-    )
-  );
-};
+class CommentDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={this.props.img} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {this.props.author}
+          </a>
+          <div className="metadata">
+            <span className="date">{this.props.timeAgo}</span>
+          </div>
+          <div className="text">{this.props.commentText}</div>
+        </div>
+      </div>
+    );
+  }
+}
 
-var _default = CommentDetail;
-exports.default = _default;
+export default CommentDetail;
