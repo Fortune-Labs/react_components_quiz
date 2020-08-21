ec47e56bddcc8259c080932bbf27cca4e85c5dc7
Fortunatus A. Wulnye <56046745+Fortune-Labs@users.noreply.github.com>
2020-08-21 15:50:53 +0000

    Update App.js

--- reactjsx/src/App.js
+++ reactjsx/src/App.js
@@ -5,74 +5,70 @@ import faker from "faker";
 import ApprovalCard from "./ApprovalCard";
 import CommentDetail from "./CommentDetail";
 
-function componentBuilder() {
-  let data = [
-    {
-      author: "Dennis Obel",
-      timeAgo: "Today at 4:45PM",
-      commentText: "Good job",
-      img: faker.image.avatar(),
-    },
-    {
-      author: "Paul Solomon",
-      timeAgo: "Today at 5:45PM",
-      commentText: "Nice work",
-      img: faker.image.avatar(),
-    },
-    {
-      author: "Peter Nortey",
-      timeAgo: "Today at 6:45PM",
-      commentText: "Great stuff",
-      img: faker.image.avatar(),
-    },
-    {
-      author: "James Asafuah",
-      timeAgo: "Today at 7:45PM",
-      commentText: "Commendable",
-      img: faker.image.avatar(),
-    },
-    {
-      author: "Persius Osei-Tutu",
-      timeAgo: "Today at 7:45PM",
-      commentText: "Commendable",
-      img: faker.image.avatar(),
-    },
-  ];
+class App extends Component {
+  componentBuilder() {
+    let data = [
+      {
+        author: "Dennis Obel",
+        timeAgo: "Today at 4:45PM",
+        commentText: "Good job",
+        img: faker.image.avatar(),
+      },
+      {
+        author: "Paul Solomon",
+        timeAgo: "Today at 5:45PM",
+        commentText: "Nice work",
+        img: faker.image.avatar(),
+      },
+      {
+        author: "Peter Nortey",
+        timeAgo: "Today at 6:45PM",
+        commentText: "Great stuff",
+        img: faker.image.avatar(),
+      },
+      {
+        author: "James Asafuah",
+        timeAgo: "Today at 7:45PM",
+        commentText: "Commendable",
+        img: faker.image.avatar(),
+      },
+      {
+        author: "Persius Osei-Tutu",
+        timeAgo: "Today at 7:45PM",
+        commentText: "Commendable",
+        img: faker.image.avatar(),
+      },
+    ];
 
-  return data
-    .filter((element) => {
-      return element.author.includes("P");
-    })
-    .map((element) => {
-      return (
+    return data
+      .filter((element) => {
+        return element.author.includes("P");
+      })
+      .map((element) => {
+        return (
+          <ApprovalCard>
+            <CommentDetail
+              author={element.author}
+              timeAgo={element.timeAgo}
+              commentText={element.commentText}
+              img={element.img}
+            />
+          </ApprovalCard>
+        );
+      });
+  }
+
+  render() {
+    return (
+      <div className="ui container">
         <ApprovalCard>
-          <CommentDetail
-            author={element.author}
-            timeAgo={element.timeAgo}
-            commentText={element.commentText}
-            img={element.img}
-          />
+          <p>Are you sure?</p>
         </ApprovalCard>
-      );
-    });
+        {this.componentBuilder()}
+      </div>
+    );
+  }
 }
 
-// convert to class-based component
+export default App;
 
-function App() {
-  return React.createElement(
-    "div",
-    {
-      className: "ui container",
-    },
-    React.createElement(
-      ApprovalCard,
-      null,
-      React.createElement("p", null, "Are you sure?")
-    ),
-    componentBuilder()
-  );
-}
-
-var _default = App;
-exports.default = _default;
